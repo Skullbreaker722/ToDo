8ba1446ba7c91a374a02a7745f02260fb3b699db
Muzammil <skullbreaker722@gmail.com>
2025-08-06 19:16:58 +0530

    Day1 - Basic Page with navbar and footer

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./MyComponents/Header";
+import Footer from "./MyComponents/Footer";
+import {Todos} from "./MyComponents/Todos";
+function App() {
+  return (
+    <>
+      <Header />
+      <Todos />
+      <Footer />
+      
+
+    </>
+  );
+}
+
+export default App;
